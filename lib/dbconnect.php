1fa45cea3e911a58824364e6e209e4be55c1a107
Jegardo <45519275+Jegardo@users.noreply.github.com>
2021-01-05 20:11:07 +0200

    dbcon

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,17 +1,20 @@
 <?php
-$user='root';
-$pass='';
 $host='localhost';
 $db = 'score4';
+require_once "db_upass.php";
 
-const ROWS = 6;
-CONST COLS=7;
+$user=$DB_USER;
+$pass=$DB_PASS;
 
 
-$mysqli = new mysqli($host, $user, $pass, $db);
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it174964/mysql/run/mysql.sock');
+} else {
+        $mysqli = new mysqli($host, $user, $pass, $db);
+}
+
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
     $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }?>
-
 
