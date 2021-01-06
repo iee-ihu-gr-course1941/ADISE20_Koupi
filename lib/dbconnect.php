4e641b58876cafdd49039e0fd5b0468c65d0a8c2
BillysKes <73298709+BillysKes@users.noreply.github.com>
2021-01-06 20:46:02 +0200

    Update dbconnect.php

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -7,11 +7,10 @@ $user=$DB_USER;
 $pass=$DB_PASS;
 
 
-if(gethostname()=='users.iee.ihu.gr') {
-	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it174964/mysql/run/mysql.sock');
-} else {
+
+
         $mysqli = new mysqli($host, $user, $pass, $db);
-}
+
 
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
