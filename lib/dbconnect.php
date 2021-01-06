ca0faf8e8223f88f343822750478237e7d1f13f8
BillysKes <73298709+BillysKes@users.noreply.github.com>
2021-01-06 20:52:43 +0200

    Update dbconnect.php

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,18 +1,17 @@
 <?php
+$user='root';
+$pass='';
 $host='localhost';
 $db = 'score4';
-require_once "db_upass.php";
 
-$user=$DB_USER;
-$pass=$DB_PASS;
-
+const ROWS = 6;
+CONST COLS=7;
 
 
-
-        $mysqli = new mysqli($host, $user, $pass, $db);
-
-
+$mysqli = new mysqli($host, $user, $pass, $db);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
     $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }?>
+
+
